4ffa5fa2c2d4cb4472020323b63d6249b73acb35
Oleksii Horban <lehagorban93@gmail.com>
2022-06-14 11:50:10 +0300

    response

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export { fetchCountries }
+    function fetchCountries(name) {
+ return fetch(
+`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
+  .then(response => {
+    return response.json()
+  })
+  
+}
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from "./fetchCountries.js"
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("#search-box")
@@ -11,24 +11,74 @@ console.log(input);
 console.log(countryList);
 console.log(countryInfo);
 
-// const contryUrl = fetch("https://restcountries.com/v3.1/name/{name}").then(
-//     (response) => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     }
-//   );
-// console.log(contryUrl);
 
-function fetchCountries() {
-    return fetch("https://restcountries.com/v3.1/name/{name}").then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
+input.addEventListener("input", debounce(findCountry, DEBOUNCE_DELAY))
+
+function findCountry(e) {
+  const findToCountry = e.target.value.trim()
+  console.log(findToCountry);
+  fetchCountries(findToCountry)
+    .then(response => {
+      if (Number(response.status) === 404) {
+                    Notify.failure("Oops, there is no country with that name");
       }
-      return response.json();
+      if (response.length > 10) {
+         Notify.failure("Too many matches found. Please enter a more specific name.")
+      }
+      if (findToCountry === "") {
+        clearMarcupList()
+        clearMarcupInfo()
+      }
+      clearMarcupList()
+        clearMarcupInfo()
+      if (response.length === 1) {        
+        renderCountry(response)
+        console.log(response.length); 
+        clearMarcupList()
+        
+      }
+      else if (response.length > 1 && response.length <= 10) {
+        renderCountryList(response)
+        
+      }
+      
     }
-  );
+  )   
+    
+   .catch(error => {
+    console.log(error);
+  })
+  
 }
-fetchCountries()
+function renderCountry(items) {
+  const marcup = items.map(({ name, capital, population, languages, flags }) =>       
+      `
+       <div class = "wrapp">
+       <img src="${flags.svg}" alt = "flag" width = 60px height = 30px>    
+       <h1 class = "title"> ${name.official}<h1>
+       </div>
+       <p>Capital: ${capital}</p>
+       <p>Population: ${population}</p>
+       <p>Languages: ${Object.values(languages)}</p>          
+      `).join("")
+  countryInfo.insertAdjacentHTML("afterbegin", marcup)      
+}
+
+function renderCountryList(items) {
+  const marcupList = items.map(({ name, flags }) =>      
+      `<li>
+       <div class = "wrapp">
+       <img src="${flags.svg}" alt = "flag" width = 30px height = 15px>    
+       <h1 class = "title"> ${name.official}<h1>
+       </div>
+       </li>          
+      `).join("")
+    countryList.insertAdjacentHTML("afterbegin", marcupList)
+}
+function clearMarcupInfo() {
+  countryInfo.innerHTML = ""
+}
+
+function clearMarcupList() {
+  countryList.innerHTML = ""
+}
